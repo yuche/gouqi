1a05efd486b3be222767c01cae27791752be709b
yuche <i@yuche.me>
2016-09-11 02:02:46 +0800

    login test

--- tests/services/api.js
+++ tests/services/api.js
@@ -3,13 +3,21 @@ import {
   setCookie
 } from '../../lib/services/api.js'
 
+import crypto from 'crypto'
 import test from 'ava'
-import {
-  sample
-} from 'lodash'
 
 test('setCookie can change header', (t) => {
-  const randObj = sample(test)
-  const request = setCookie(randObj)
-  t.deepEqual(request.defaults.headers.common['Cookie'], randObj)
+  const randomStr = crypto.randomBytes(20).toString('hex')
+  const request = setCookie(randomStr)
+  t.deepEqual(request.defaults.headers.common['Cookie'], randomStr)
+})
+
+test('email login works', async (t) => {
+  const { config } = await login('苟', '岂')
+  t.is(config.url, 'http://music.163.com/weapi/login/')
+})
+
+test('should login algorithm works', async (t) => {
+  const { data } = await login('13812345678', 'fuckgfw')
+  t.truthy(data)
 })
