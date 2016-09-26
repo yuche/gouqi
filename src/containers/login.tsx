5bfc8a9b8c391e589c42878a88b512bb289baa15
yuche <i@yuche.me>
2016-09-26 18:56:20 +0800

    start write some UI

--- src/containers/login.tsx
+++ src/containers/login.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import * as React from 'react'
 import {
   Text,
   TextInput,
   View
 } from 'react-native'
 
-export default class Login extends React.Component<any, any> {
-  public render() {
+interface IState {
+  username: string,
+  password: string
+}
+
+class Login extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleUsernameChange(text: string) {
+    this.setState({username: text})
+  }
+
+  handlePasswordChange(text: string) {
+    this.setState({password: text})
+  }
+
+  render() {
     return (
       <View style={{padding: 10}}>
-        <TextInput />
+        <TextInput
+          onChangeText={this.handleUsernameChange}
+        />
+        <TextInput
+          onChangeText={this.handlePasswordChange}
+        />
         <Text>登录</Text>
       </View>
     )
   }
 }
+
+export default Login
